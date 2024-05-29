86f05498604d286cbebd3e9b7a25bbf78b1a3a50
SebastianHdzMiranda <sebashm68@gmail.com>
2024-05-29 17:18:23 -0600

    Heredar y extender un type

--- src/types/types.ts
+++ src/types/types.ts
@@ -5,11 +5,8 @@ export type Guitar = {
     description: string
     price: number
 }
-export type CartItem = {
-    id : number
-    name: string
-    image: string
-    description: string
-    price: number
+
+// Heredando propiedades de Guitar
+export type CartItem = Guitar & {
     quantify: number
 }
