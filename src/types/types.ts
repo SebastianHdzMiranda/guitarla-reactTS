ef2894a7d1fa660534d0696f7dcb860e68f3a2d0
SebastianHdzMiranda <sebashm68@gmail.com>
2024-05-29 17:29:16 -0600

    Utility types en TS, Pick y Omit

--- src/types/types.ts
+++ src/types/types.ts
@@ -10,3 +10,13 @@ export type Guitar = {
 export type CartItem = Guitar & {
     quantify: number
 }
+
+// Heredamos las props de Guitar que seleccionamos en Pick
+export type PickItem = Pick< Guitar, 'id' | 'name' | 'price'> & {
+    quantify: number
+}
+
+// Heredamos las props de Guitar omitiendo las declaradas en Omit
+export type OmitItem = Omit< Guitar, 'id' | 'description'> & {
+    quantify: number
+}
